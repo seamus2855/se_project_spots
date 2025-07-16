466f1d1c6acd4aecc4841fc247d0f95498e63e96
Seamus Ryan <seamusryan787@gmail.com>
2025-07-16 14:08:15 -0400

    finished stage 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileBtn = document.querySelector(".profile_edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
