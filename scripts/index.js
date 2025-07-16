bc701b36fb19b4a0cda497b2fbbc3f7149a95e09
Seamus Ryan <seamusryan787@gmail.com>
2025-07-16 13:58:24 -0400

    finished stage 4

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,21 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
-const newPostBtnModal = document.querySelector("#new-post-modal");
+const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add(modal_is - opened);
+  editProfileModal.classList.add("modal_is-opened");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove(modal_is - opened);
+  editProfileModal.classList.remove("modal_is-opened");
 });
 
-newPostBtnBtn.addEventListener("click", function () {
-  newPostModal.classList.add(modal_is - opened);
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove(modal_is - opened);
+  newPostModal.classList.remove("modal_is-opened");
 });
